4aa9661aefe8914bdf98bfe2b5690ab4ec8e0610
KefahWambai <kefah.ngwei@student.moringaschool.com>
2024-01-10 09:22:15 +0300

    first commit

---
+++ hello.go
@@ -0,0 +1,24 @@
+package main
+import "fmt"
+
+func main() {
+
+    var Welcome string = "Welcome"
+    fmt.Println(Welcome)
+    
+    var Name string = "Kefs" + "" + "Dods"
+    fmt.Println(Name)
+
+    myVar := "Kefah"
+    fmt.Println(myVar)
+
+    age := 27
+    fmt.Println(age) 
+
+    employed := false
+    fmt.Println(employed)
+
+    // age, employed := 27, false
+    // fmt.Println(age, employed)
+}
+
